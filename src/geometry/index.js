d66903a441a3f245cf541aacecae0b461defc2f4
Nikos Andronikos <dodgeyhack@gmail.com>
2017-02-14 21:19:57 +1100

    export degree to radius constants

--- src/geometry/index.js
+++ src/geometry/index.js
@@ -2,4 +2,4 @@ export {PositionVector} from './positionVector';
 export {Rect} from './rect';
 export {Point} from './point';
 export {Vector2d} from './vector2d';
-export {degToRad, radToDeg} from './utils';
+export {degToRad, radToDeg, DEG_TO_RAD, RAD_TO_DEG} from './utils';
--- src/geometry/utils.js
+++ src/geometry/utils.js
@@ -1,5 +1,5 @@
-export const DEG_TO_RAD = Math.PI / 180;
-export const RAD_TO_DEG = 180 / Math.PI;
+export const DEG_TO_RAD = (Math.PI / 180);
+export const RAD_TO_DEG = (180 / Math.PI);
 
 export function degToRad(degrees) {
     return degrees * DEG_TO_RAD;
