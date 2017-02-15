9e6d7de26f673268af963552ed84792f9cf7e222
Nikos Andronikos <dodgeyhack@gmail.com>
2017-02-15 21:50:06 +1100

    Tidy up Keyboard

--- src/input/input.js
+++ src/input/input.js
@@ -1,18 +1,17 @@
-// Kind of like a singleton
 export const Keyboard  = {
-    "KEY_ESC": 27,
-    "KEY_UP": 38,
-    "KEY_DOWN": 40,
-    "KEY_LEFT": 37,
-    "KEY_RIGHT": 39,
-    "KEY_W": 87,
-    "KEY_A": 65,
-    "KEY_D": 68,
-    "KEY_SPACE": 32,
-    "keys": [],
-    "ignoreKeys": [],
-    "nKeysDown": 0,
-    "keydown_handler": function(evt) {
+    KEY_ESC:    27,
+    KEY_UP:     38,
+    KEY_DOWN:   40,
+    KEY_LEFT:   37,
+    KEY_RIGHT:  39,
+    KEY_W:      87,
+    KEY_A:      65,
+    KEY_D:      68,
+    KEY_SPACE:  32,
+    keys:       [],
+    ignoreKeys: [],
+    nKeysDown:  0,
+    keydown_handler: function(evt) {
         if (!Keyboard.ignoreKeys[evt.keyCode]) {
             if (!Keyboard.keys[evt.keyCode]) Keyboard.nKeysDown ++;
             Keyboard.keys[evt.keyCode] = true;
@@ -26,7 +25,7 @@ export const Keyboard  = {
             evt.preventDefault();
         }
     },
-    "keyup_handler": function(evt) {
+    keyup_handler: function(evt) {
         // TODO: Remove from array to minimise memory consumption,
         // or only track particular keys.
         if (Keyboard.ignoreKeys[evt.keyCode]) {
@@ -44,17 +43,17 @@ export const Keyboard  = {
             evt.preventDefault();
         }
     },
-    "pressed": function(keyCode) {
+    pressed: function(keyCode) {
         return Keyboard.keys[keyCode] === true;
     },
-    "anyKeyDown": function() {
+    anyKeyDown: function() {
         return Keyboard.nKeysDown > 0;
     },
-    "ignoreUntilReleased": function(keyCode) {
+    ignoreUntilReleased: function(keyCode) {
         if (Keyboard.keys[keyCode]) {
             Keyboard.nKeysDown --;
             Keyboard.keys[keyCode] = false;
             Keyboard.ignoreKeys[keyCode] = true;
         }
     }
-}
+};
