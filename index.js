8fd78149ba6130b146351e9a2a08aef0a6438c2e
Nikos Andronikos <dodgeyhack@gmail.com>
2017-02-17 21:45:54 +1100

    Add index for when importing via modules

---
+++ index.js
@@ -0,0 +1,6 @@
+export * from './src/assert/';
+export * from './src/geometry/';
+export * from './src/dataStruct/';
+export * from './src/input/';
+export * from './src/pattern/';
+export * from './src/game/';
