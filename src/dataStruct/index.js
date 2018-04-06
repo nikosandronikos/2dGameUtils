7c8a50d7cdf8867b0e35e311c1fbb3903c325857
Nikos Andronikos <nikos.andronikos@gmail.com>
2018-04-06 14:32:49 +1000

    Add binary search functions to exports

--- src/dataStruct/index.js
+++ src/dataStruct/index.js
@@ -1,3 +1,4 @@
 export {PriorityQueue} from './PriorityQueue';
 export {quadTree} from './quadTree';
 export {LinkedList} from './linkedList';
+export {binarySearch, binarySearchClosest} from './binarySearch';
