2b2fa8e11474b89feee88853295a5b67e1b15829
Nikos Andronikos <nikos.andronikos@gmail.com>
2018-04-06 14:47:54 +1000

    Add createFromPointAndAngle to PositionVector

--- src/geometry/positionVector.js
+++ src/geometry/positionVector.js
@@ -13,7 +13,14 @@ export class PositionVector {
             p2.x - p1.x, p2.y - p1.y
         );
     }
-    
+
+    static createFromPointAndAngle(p1, angle, length=1) {
+        const pv = Object.create(PositionVector.prototype);
+        pv.position = new Point(p1.x, p1.y);
+        pv.vector = Vector2d.createFromAngle(angle, length) ;
+        return pv;
+    }
+   
     copy() {
         return new PositionVector(this.position.x, this.position.y, this.vector.x, this.vector.y);
     }
